4f6646a82c834f78be752f30a853a77ffbac4b89
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-12 14:20:30 +0000

    Hello , printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * Print exactly "Programming is like building a multilingual puzzle
+ *
+ * Auther : by me
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
