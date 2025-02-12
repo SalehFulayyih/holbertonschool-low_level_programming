7859d72f8b337ea6c39abb8ecec5df466c8bab22
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-12 14:30:31 +0000

    Hello , printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * Print exactly with proper grammar, but the outcome is a piece of art,
+ * main - Entry point
  *
- * Auther : by me
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
