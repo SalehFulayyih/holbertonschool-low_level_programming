48ae78ab006532afdbff1d1f50f8f921d3264658
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-16 12:27:32 +0000

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: This program prints all single digit numbers of bas
+ * 10 starting from 0
+ * Return: 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	return (0);
+}
