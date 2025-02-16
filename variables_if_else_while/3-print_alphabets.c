f985b6b657914d17fa0a83952fd1f9968e0d4b8e
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-16 12:03:29 +0000

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: This program prints the alphabet in lowercase and
+ * then in uppercase
+ * Return: 0
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	    putchar(letter)
+	putchar('\n');
+	return (0);
+}
