f6a8d9ca66cb22d9314720880f9fe0badca14c29
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-16 13:00:45 +0000

    Smile in the mirror

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -12,9 +12,9 @@
 
 int main(void)
 {
-        char ltr;
-
-        for (ltr = 'z'; ltr >= 'a'; ltr--)
+	char ltr;
+	
+	for (ltr = 'z'; ltr >= 'a'; ltr--)
 		putchar(ltr);
 	putchar('\n');
         return (0);
