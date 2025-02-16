c75482a618a57865ad7807803f4e52e45ea2640f
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-16 12:56:52 +0000

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: This program that prints the lowercase alphabet in reverse, followed by a new line
+ * Return: 0
+*/
+
+int main(void)
+{
+        char ltr;
+
+        for (ltr = 'z'; ltr >= 'a'; ltr--)
+                putchar(ltr);
+        putchar('\n');
+        return (0);
+}
