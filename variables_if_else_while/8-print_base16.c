0bbdea2510e6dcb408c8dede36e50c276c16834c
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-16 13:20:08 +0000

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: This program prints all the numbers of base 16 in
+ * lowercase
+ * Return: 0
+*/
+
+int main(void)
+{
+        char num, ltr;
+
+        for (num = '0'; num <= '9'; num++)
+                putchar(num);
+        for (ltr = 'a'; ltr <='f'; ltr++)
+                putchar(ltr);
+        putchar('\n');
+        return (0);
+}
