27ff2949789aa6f7da527d9381c4123c5382dedf
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-16 12:59:03 +0000

    Smile in the mirror

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 /**
  * main - main block
- * Description: This program that prints the lowercase alphabet in reverse, followed by a new line
+ * Description: This program that prints the lowercase alphabet in reverse
  * Return: 0
 */
 
@@ -15,7 +15,7 @@ int main(void)
         char ltr;
 
         for (ltr = 'z'; ltr >= 'a'; ltr--)
-                putchar(ltr);
-        putchar('\n');
+		putchar(ltr);
+	putchar('\n');
         return (0);
 }
