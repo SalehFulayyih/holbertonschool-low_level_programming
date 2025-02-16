335692bb264f16ccfd22c03eaa4b9b03c76a9f9d
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-16 13:32:11 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: This program prints all possible of single-digit like (1, 3, )
+ * Return: 0
+*/
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putcahr(num);
+		putchar(', ');
+	putchar('\n')
+	return(0);
+}
