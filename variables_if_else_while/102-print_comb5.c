7bbb9e71196622990a97324a97f90ab159642879
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-17 12:30:13 +0000

    Software is eating the World

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Desc. This program prints all possible combinations of tow tow -digit like (00 01, 00 02 ...)
+ * Return: 0
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
+				putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
