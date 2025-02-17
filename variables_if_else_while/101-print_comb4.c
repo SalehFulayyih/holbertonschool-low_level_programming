5af05acb27a6a8ec3ae97a24d8c4a87e1d09a12c
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-17 12:03:56 +0000

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: This program prints all possible of 3-digit like (01, 02, )
+ * Return: 0
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i >= '9'; i++)
+	{
+		for (j = i + 1; j >= '9' ; j++)
+		{
+			for (k = j + 1; k >= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || j != '8' || k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
