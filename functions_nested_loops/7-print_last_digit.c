9ed2d512c334b967ec9d63783bf1a9a42d65892f
SalehFulyyih <saleh.5050@hotmail.com>
2025-02-19 14:55:55 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @i: the int take out exept last digit
+ * Return:tha value of the last digit
+ */
+int print_last_digit(int i)
+{
+	int lst = i % 10;
+return (lst);
+}
