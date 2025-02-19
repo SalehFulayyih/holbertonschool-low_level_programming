06f2c203496282909af3dd75bc06892ec157ac4f
SalehFulyyih <saleh.5050@hotmail.com>
2025-02-19 13:13:47 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphbet_x10 - Print ten times the alphbet in lowercase
+ * Return:0
+ */
+
+void print_alphbet_x10(void)
+{
+	char letter = 'a';
+	int i;
+
+
+	for (i = 1; i <= 10; i++)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
+
