469287ea80168a9ae264038db480f3caa842d96b
SalehFulyyih <saleh.5050@hotmail.com>
2025-02-19 12:07:02 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Description: Prints _putchar
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
