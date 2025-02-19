91e88af5dd77954fc184511b1e34d62dc7d6e75d
SalehFulyyih <saleh.5050@hotmail.com>
2025-02-19 13:59:32 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks if char is alpha or not
+ * @c: the char to check
+ * Return: 1 if c is alpha, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= "A" && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
