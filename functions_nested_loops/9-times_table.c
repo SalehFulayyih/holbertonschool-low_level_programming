324efdc281609c70e2af343adc8aa7afe5cb18f3
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-20 14:39:07 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,17 +13,19 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (j > 0)
+			result = i * j;
+			if (result > 9)
 			{
-				_putchar (',');
+				_putchar ((result / 10) + '0');
+			}
+			else
+				_putchar(' ');
+			_putchar((result % 10) + '0');
+			if (j < 9)
+			{
+				_putchar(',');
 				_putchar(' ');
 			}
-			result = i * j;
-			if (result <= 9)
-				_putchar(' ');
-			else
-				_putchar((result / 10) + '0');
-			_putchar((result % 10) + '0');
 		}
 	_putchar('\n');
 	}
