d8f8423819dd245d7c5c578d08185d3dfbb7e008
SalehFulayyih <saleh.5050@hotmail.com>
2025-02-27 12:21:34 +0000

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <<stdio.h>>
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		puts(*str++);
+
+	puts('\n');
+}
