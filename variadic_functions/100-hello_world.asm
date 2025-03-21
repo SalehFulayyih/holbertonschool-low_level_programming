5d1013d844c505602c30c58565528653385b467a
SalehFulayyih <saleh.5050@hotmail.com>
2025-03-21 13:40:41 +0300

    Update 100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,23 +1,21 @@
-%define sys_write 1
-%define stdout 1
-
-%define sys_exit 60
-%define success 0
-
-%define nl 10
-
-section .data
-
-	message db "Hello, Holberton", nl
+; File: 100-hello_holberton.asm
+; Auth: Brennan D Baraban
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
 
 section .text
-	global main
+   global main
+
 main:
-	mov rax, sys_write
-	mov rdi, stdout
-	mov rsi, message
-	mov rdx, 17
-	syscall
-	mov    rax, sys_exit
-	mov    rdi, success
-	syscall
+   mov edx,len
+   mov ecx,msg
+   mov ebx,1
+   mov eax,4
+   int 0x80
+
+   mov eax,0
+   int 0x80
+
+section .data
+   msg: db 'Hello, Holberton', 0xa
+   len: equ $ - msg
