dd7a2a76bb79375a3951ec1d0f87ce324624fa6f
SalehFulayyih <saleh.5050@hotmail.com>
2025-03-21 11:18:21 +0000

    Edit

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,16 +1,22 @@
 section .data
-	msg db "Hello, World",10
+    hello db 'Hello, World', 0xa  ; The string to print, followed by a newline (0xa)
 
-	section .text
-	global main
+section .text
+    global _start
 
-main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
-	syscall
+_start:
+    ; sys_write (syscall number 1)
+    ; File descriptor 1 (stdout)
+    ; Address of the string to be printed
+    ; Length of the string
+    mov rax, 1           ; syscall number for sys_write (1)
+    mov rdi, 1           ; file descriptor 1 (stdout)
+    mov rsi, hello       ; address of the string to print
+    mov rdx, 14          ; length of the string (13 characters + 1 for the newline)
+    syscall              ; make the syscall
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
+    ; Exit the program
+    mov rax, 60          ; syscall number for sys_exit (60)
+    xor rdi, rdi         ; exit code 0
+    syscall              ; make the syscall
+
