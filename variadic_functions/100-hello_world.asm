69f49b753e889fe9a0574f59725bfda49ffa2b95
SalehFulayyih <saleh.5050@hotmail.com>
2025-03-21 14:19:47 +0300

    Update 100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,17 +1,16 @@
 section .data
-    msg db "Hello, World", 0xA   ; The string with a newline character
-    len equ $ - msg              ; Calculate the length of the string
+	msg db "Hello, Holberton",10
 
-section .text
-    global _start
+	section .text
+	global main
 
-_start:
-    mov rax, 1                   ; System call number for write
-    mov rdi, 1                   ; File descriptor (stdout)
-    mov rsi, msg                 ; Address of the string to print
-    mov rdx, len                 ; Length of the string
-    syscall                      ; Make the system call
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17
+	syscall
 
-    mov rax, 60                  ; System call number for exit
-    xor rdi, rdi                 ; Exit code 0
-    syscall                      ; Make the system call
+	mov rax, 60
+	mov rdi, 0
+	syscall
